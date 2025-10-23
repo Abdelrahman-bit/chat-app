e606dcd47128095c9bc94743b680ab476f31a32e
Abdelrahman-bit <abdolela670@gmail.com>
2025-10-23 23:38:41 +0300

    just a try

--- backend/src/utils/soket.js
+++ backend/src/utils/soket.js
@@ -15,18 +15,6 @@ const io = new Server(server, {
 });
 
 // Track connected sockets per userId so we can compute which userIds are online
-const userSocketCount = new Map(); // userId -> number of sockets
-
-function emitOnlineUsers() {
-	const onlineUsers = Array.from(userSocketCount.keys());
-	try {
-		io.emit("onlineUsers", onlineUsers);
-		console.log("Emitted onlineUsers:", onlineUsers);
-	} catch (e) {
-		console.error("Failed to emit onlineUsers", e);
-	}
-}
-
 io.on("connection", (socket) => {
 	console.log("A user connected", socket.id);
 
@@ -36,34 +24,11 @@ io.on("connection", (socket) => {
 		if (userId) {
 			socket.join(userId);
 			console.log(`Socket ${socket.id} joined room ${userId}`);
-
-			// increment socket count for this user
-			const prev = userSocketCount.get(userId) || 0;
-			userSocketCount.set(userId, prev + 1);
-			// broadcast updated list
-			emitOnlineUsers();
 		}
-	} catch (e) {
-		console.error("Error during socket connection auth handling", e);
-	}
+	} catch (e) {}
 
 	socket.on("disconnect", () => {
 		console.log("A user disconnected", socket.id);
-		try {
-			const userId = socket.handshake?.auth?.userId;
-			if (userId) {
-				const prev = userSocketCount.get(userId) || 0;
-				if (prev <= 1) {
-					userSocketCount.delete(userId);
-				} else {
-					userSocketCount.set(userId, prev - 1);
-				}
-				emitOnlineUsers();
-			}
-		} catch (e) {
-			console.error("Error during socket disconnect handling", e);
-		}
 	});
 });
-
 export { app, server, io };
